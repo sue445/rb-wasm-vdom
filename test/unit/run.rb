9bd198a3e7a58096ba46071442e004adf03accfc
sue445 <sue445@sue445.net>
2026-06-15 23:27:38 +0900

    :cop: rubocop --autocorrect --only Layout/IndentationConsistency

--- test/unit/run.rb
+++ test/unit/run.rb
@@ -3,15 +3,15 @@ module MiniTestMock
   def assert(condition, message = "Assertion failed")
     return if condition
 
-      raise "Failure: #{message}"
+    raise "Failure: #{message}"
     
   end
 
   def assert_equal(expected, actual, message = nil)
     return if expected == actual
 
-      msg = message || "Expected #{expected.inspect}, but got #{actual.inspect}"
-      raise "Failure: #{msg}"
+    msg = message || "Expected #{expected.inspect}, but got #{actual.inspect}"
+    raise "Failure: #{msg}"
     
   end
 end
