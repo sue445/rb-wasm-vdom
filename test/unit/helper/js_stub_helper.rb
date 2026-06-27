06c7dcb7394c8b6a45f00628d84008100680c693
sue445 <sue445@sue445.net>
2026-06-27 20:03:08 +0900

    Add js_stub_helper.rb

---
+++ test/unit/helper/js_stub_helper.rb
@@ -0,0 +1,46 @@
+module JsStubHelper
+  def with_js_global_stub
+    console = ConsoleErrorSpy.new
+    global = JsGlobalStub.new(console)
+
+    js_was_defined = Object.const_defined?(:JS)
+    Object.const_set(:JS, Module.new) unless js_was_defined
+
+    original_global = JS.method(:global) if JS.respond_to?(:global)
+    JS.define_singleton_method(:global) { global }
+
+    yield console
+  ensure
+    if original_global
+      JS.define_singleton_method(:global) { original_global.call }
+    elsif js_was_defined
+      JS.singleton_class.remove_method(:global) if JS.respond_to?(:global)
+    else
+      Object.send(:remove_const, :JS)
+    end
+  end
+
+  class ConsoleErrorSpy
+    attr_reader :messages
+
+    def initialize
+      @messages = []
+    end
+
+    def error(message)
+      @messages << message
+    end
+  end
+
+  class JsGlobalStub
+    def initialize(console)
+      @console = console
+    end
+
+    def [](key)
+      return @console if key == :console
+
+      nil
+    end
+  end
+end
